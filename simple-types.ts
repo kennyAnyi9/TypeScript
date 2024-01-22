b654cb6c17513f635b943b8a352a01919ca52c76
kennyAnyi9 <kennyanyi9@gmail.com>
2024-01-22 08:10:52 +0000

    feat: update with simple types

---
+++ simple-types.ts
@@ -0,0 +1,14 @@
+//primitive datatypes are boolean, string and number/ other ones are bigint and symbol
+
+//type assignment
+
+//explicit (specifying the type)
+
+let myName: string = "Kennedy Anyidoho";
+console.log(myName, typeof myName); // this will log : Kennedy Anyidoho string
+
+//implicit (leaiving it to typescript to figure out the  so we say typescript infers the value)
+
+let Name = "Kennedy Anyidoho";
+
+console.log(Name, typeof Name); // this will log : Kennedy Anyidoho string
